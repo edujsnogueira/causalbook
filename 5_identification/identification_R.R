60f441de68902ea8ce99d3d29d9dcad4f5c1b99d
Eduardo Nogueira <edujsnogueira@gmail.com>
2023-09-21 16:38:46 -0300

    atualização

--- 5_identification/identification_R.R
+++ 5_identification/identification_R.R
@@ -24,11 +24,11 @@ dfavo <- dfavo %>%
   arrange(Date)
 
 ggplot(dfavo, aes(y = `Total Volume (Millions)`, x = AveragePrice)) + 
-  geom_point(size = 1)+
-  theme_pubr()+
-  theme(text         = element_text(size = 16, family="Garamond"),
-          axis.title.x = element_text(size = 16, family="Garamond"),
-          axis.title.y = element_text(size = 16, family= "Garamond")) +
+  geom_point(size = 1) +
+  theme_pubr() +
+  theme(text         = element_text(size = 16, family = "Garamond"),
+          axis.title.x = element_text(size = 16, family = "Garamond"),
+          axis.title.y = element_text(size = 16, family = "Garamond")) +
   labs(y = "Total Avocados Sold (Millions)",
        x = "Average Avocado Price",
        title = "",
@@ -37,13 +37,13 @@ ggsave('avocado.pdf', width = 6, height = 5, device = cairo_pdf)
 
 ggplot(dfavo %>% mutate(isolate = row_number() %in% 4:5), 
        aes(y = `Total Volume (Millions)`, x = AveragePrice, alpha = isolate)) + 
-  geom_point(size = 2)+
-  theme_pubr()+
+  geom_point(size = 2) +
+  theme_pubr() +
   guides(alpha = FALSE) + 
-  scale_alpha_manual(values = c(0,1))+
-  theme(text         = element_text(size = 16, family="Garamond"),
-        axis.title.x = element_text(size = 16, family="Garamond"),
-        axis.title.y = element_text(size = 16, family= "Garamond")) +
+  scale_alpha_manual(values = c(0,1)) +
+  theme(text         = element_text(size = 16, family = "Garamond"),
+        axis.title.x = element_text(size = 16, family = "Garamond"),
+        axis.title.y = element_text(size = 16, family = "Garamond")) +
   geom_label_repel(aes(label = as.character(Date)), direction = 'y') + 
   labs(y = "Total Avocados Sold (Millions)",
        x = "Average Avocado Price",
@@ -57,13 +57,13 @@ ggplot(dfavo %>% slice(5:12) %>%
          mutate(yearmonth = ifelse(yearmonth == 201503, "Mar. 2015","Feb. 2015")) %>%
          mutate(yearmolabel = ifelse(row_number() %in% c(1,5),yearmonth,NA)),
        aes(y = `Total Volume (Millions)`, x = AveragePrice)) + 
-  geom_point(size = 2)+
+  geom_point(size = 2) +
   geom_line(aes(linetype = factor(yearmonth))) + 
-  theme_pubr()+
+  theme_pubr() +
   guides(linetype = FALSE) + 
-  theme(text         = element_text(size = 16, family="Garamond"),
-        axis.title.x = element_text(size = 16, family="Garamond"),
-        axis.title.y = element_text(size = 16, family= "Garamond")) +
+  theme(text         = element_text(size = 16, family = "Garamond"),
+        axis.title.x = element_text(size = 16, family = "Garamond"),
+        axis.title.y = element_text(size = 16, family = "Garamond")) +
   geom_label_repel(aes(label = yearmolabel)) + 
   labs(y = "Total Avocados Sold (Millions)",
        x = "Average Avocado Price",
@@ -88,9 +88,9 @@ ggplot(df %>% filter(Hair == "Brown"), aes(x = logIncome, linetype = Hair)) +
                geom = 'line', size = 1) +
   theme_pubr() + 
   labs(x = "Log Income", y = "Density") + 
-  theme(text         = element_text(size = 16, family="Garamond"),
-        axis.title.x = element_text(size = 16, family="Garamond"),
-        axis.title.y = element_text(size = 16, family= "Garamond"),
+  theme(text         = element_text(size = 16, family = "Garamond"),
+        axis.title.x = element_text(size = 16, family = "Garamond"),
+        axis.title.y = element_text(size = 16, family = "Garamond"),
         legend.position = c(.2,.8),
         legend.background = element_rect())
 ggsave('hair1.pdf', width = 6, height = 5, device = cairo_pdf)
@@ -117,7 +117,7 @@ df %>%
 # Data extracted from published graph using https://apps.automeris.io/wpd/
 alc_mort <- read_csv('alcohol_mortality_digitizer.csv') %>%
   # Identify points by sets of three
-  mutate(point = floor((row_number()-1)/3)) %>%
+  mutate(point = floor((row_number() - 1)/3)) %>%
   group_by(point) %>%
   # The actual point is the first one
   mutate(datactr = row_number() == 1) %>%
@@ -133,9 +133,9 @@ ggplot(alc_mort %>% filter(datactr), aes(x = X, y = Y,
   geom_hline(aes(yintercept = 1), size = .3, linetype = 'dashed') + 
   theme_pubr() + 
   labs(x = "Usual Alcohol Consumption (Grams/wk)", y = "Hazard Ratio (1 = no effect)") + 
-  theme(text         = element_text(size = 16, family="Garamond"),
-        axis.title.x = element_text(size = 16, family="Garamond"),
-        axis.title.y = element_text(size = 16, family= "Garamond"))
+  theme(text         = element_text(size = 16, family = "Garamond"),
+        axis.title.x = element_text(size = 16, family = "Garamond"),
+        axis.title.y = element_text(size = 16, family = "Garamond"))
 ggsave('original_alcohol_mortality.pdf', width = 6, height = 5, device = cairo_pdf)
 
 # Alcohol and Maleness
@@ -150,7 +150,7 @@ ggplot(alc_male, aes(x = X, y = Y)) +
   geom_hline(aes(yintercept = 1), size = .3, linetype = 'dashed') + 
   theme_pubr() + 
   labs(x = "Drinks Per Week", y = "Odds Ratio (1 = no effect)") + 
-  theme(text         = element_text(size = 16, family="Garamond"),
-        axis.title.x = element_text(size = 16, family="Garamond"),
-        axis.title.y = element_text(size = 16, family= "Garamond"))
+  theme(text         = element_text(size = 16, family = "Garamond"),
+        axis.title.x = element_text(size = 16, family = "Garamond"),
+        axis.title.y = element_text(size = 16, family = "Garamond"))
 ggsave('auld_alcohol_male.pdf', width = 6, height = 5, device = cairo_pdf)
