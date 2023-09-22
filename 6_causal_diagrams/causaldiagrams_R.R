f17a01fa7e1dad56a66d4fbcb718c8440e13fb55
Eduardo Nogueira <edujsnogueira@gmail.com>
2023-09-22 01:01:02 -0300

    atualização

--- 6_causal_diagrams/causaldiagrams_R.R
+++ 6_causal_diagrams/causaldiagrams_R.R
@@ -12,9 +12,9 @@ ggplot(crime4, aes(x = polpc, y = crmrte)) +
   theme_pubr() + 
   scale_x_log10() + 
   scale_y_log10() + 
-  theme(text         = element_text(size = 13, family="Garamond"),
-        axis.title.x = element_text(size = 13, family="Garamond"),
-        axis.title.y = element_text(size = 13, family= "Garamond")) +
+  theme(text         = element_text(size = 13, family = "Garamond"),
+        axis.title.x = element_text(size = 13, family = "Garamond"),
+        axis.title.y = element_text(size = 13, family = "Garamond")) +
   labs(x = "Police per Capita (log scale)",
        y = "Crime Rate (log scale)")
-ggsave('crime1.pdf', width = 6, height = 5, device=cairo_pdf)
+ggsave('crime1.pdf', width = 6, height = 5, device = cairo_pdf)
